911601e781fcd81a7209ea335db608cc97a327ad
Sebastiaan van Parijs <svparijs@tryweb.nl>
2013-01-18 14:53:18 +0100

    [BUGFIX] #4: Disable composer to get a webroot

--- .heroku/compile.sh
+++ .heroku/compile.sh
@@ -11,7 +11,7 @@ APP_DIR="$( dirname "${REPO_DIR}" )"
 cd $REPO_DIR
 unset GIT_DIR
 curl -s http://getcomposer.org/installer | $APP_DIR/bin/php
-$APP_DIR/bin/php composer.phar install --prefer-dist
+# $APP_DIR/bin/php composer.phar install --prefer-dist
 
 # cleanup .git directory
 find $REPO_DIR -maxdepth 1 -name .git -exec rm -rf {} \;
